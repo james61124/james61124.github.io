24ef63cddf435b98664ea1c418fbf661846236e2
james61124 <james61124@gmail.com>
2023-02-06 00:39:33 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = { 
-  publicPath: process.env.NODE_ENV === 'production' 
-  ? '/james_website/' //為github的專案名稱 
-  : '/' 
+  // publicPath: process.env.NODE_ENV === 'production' 
+  // ? '/james_website/' //為github的專案名稱 
+  // : '/' 
+  publicPath: '/james61124.github.io/dist/'
  }
